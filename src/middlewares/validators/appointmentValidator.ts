04f502b3d3a16b7d62b22968394d34c35c846dd9
Ibrahim-fci <ibrahimismail00000@gmail.com>
2024-02-25 00:33:28 +0200

    Update appointmentValidator.ts

--- src/middlewares/validators/appointmentValidator.ts
+++ src/middlewares/validators/appointmentValidator.ts
@@ -6,6 +6,9 @@ export default {
     check("doctorId").notEmpty().withMessage("doctorId name is required"),
     check("reason").notEmpty().withMessage("reason  is required"),
     check("date").isDate().withMessage("appointment date is required"),
+    check("from").notEmpty().withMessage("from time is required"),
+    check("to").notEmpty().withMessage("to time is required"),
+    check("day").notEmpty().withMessage("day is required and must be number between 0 to 6"),
 
     validatorMiddeleware,
   ],
